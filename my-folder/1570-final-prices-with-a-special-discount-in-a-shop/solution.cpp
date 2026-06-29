eb314ff06ec37e36e27dee06d0ca0e78f063a41e
AbanoubEmadM <83979832+AbanoubEmadM@users.noreply.github.com>
2026-06-29 15:47:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.3 MB (28.27%)

--- my-folder/1570-final-prices-with-a-special-discount-in-a-shop/solution.cpp
+++ my-folder/1570-final-prices-with-a-special-discount-in-a-shop/solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-      vector<int> s1, discount;
+        stack<int> st;
+        vector<int> ans = prices;
 
-    for (int i = 0; i < prices.size(); i++) {
-       for (int j = i+1; j < prices.size(); j++) {
-           if (prices[i] >= prices[j]) {
-               discount.push_back(prices[i] - prices[j]);
-               break;
-           }
-           if (j == prices.size() - 1) {discount.push_back(prices[i]);}
-       }
-    }
-    discount.push_back(prices[prices.size()-1]);
-    return discount;
+        for (int i = 0; i < prices.size(); i++)
+        {
+            while(!st.empty() && prices[st.top()] >= prices[i])
+            {
+                ans[st.top()] -= prices[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
     }
 };
